d53064d0292ed50ac571f6bbe45563396f1c05d4
2E1HNK <2e1hnk@deadbeef.cafe>
2020-11-16 16:16:14 +0000

    added bramus router dependency and server start scripts

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b479310f61195ad6ec871e9532bf25585af50d6e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b479310f61195ad6ec871e9532bf25585af50d6e',
+    ),
+    'bramus/router' => 
+    array (
+      'pretty_version' => '1.5',
+      'version' => '1.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6adc0182dc6b0ebc22fd10543f65a36de10d8c05',
+    ),
+  ),
+);
